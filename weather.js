d26c34ff3f97c15c47b042ae3749785bc070ebbb
Kushal Vithal <kushal17vithal@gmail.com>
2024-06-17 23:35:13 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,75 @@
+//https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&precipitation_unit=inch&timeformat=unixtime
+
+import axios from "axios";
+export function getWeather(lat, lon, timezone) {
+  return axios
+    .get(
+      "https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&precipitation_unit=inch&timeformat=unixtime",
+      {
+        params: {
+          latitude: lat,
+          longitude: lon,
+          timezone: timezone,
+        },
+      }
+    )
+
+    .then(({ data }) => {
+      return {
+        current: parseCurrentWheather(data),
+        daily: parseDailyWheather(data),
+        hourly: parseHourlyWheather(data),
+      };
+    });
+}
+
+function parseCurrentWheather({ current, daily }) {
+  const {
+    temperature_2m: currentTemp,
+    wind_speed_10m: windSpeed,
+    weather_code: iconCode,
+  } = current;
+
+  const {
+    temperature_2m_max: [maxTemp],
+    temperature_2m_min: [minTemp],
+    apparent_temperature_max: [maxFeelsLike],
+    apparent_temperature_min: [minFeelsLike],
+    precipitation_sum: [precip],
+  } = daily;
+  return {
+    currentTemp,
+    highTemp: Math.round(maxTemp),
+    lowTemp: Math.round(minTemp),
+    highFeelsLike: Math.round(maxFeelsLike),
+    lowFeelsLike: Math.round(minFeelsLike),
+    windSpeed: Math.round(windSpeed),
+    precip: Math.round(precip * 100) / 100,
+    iconCode,
+  };
+}
+
+function parseDailyWheather({ daily }) {
+  return daily.time.map((time, index) => {
+    return {
+      timestamp: time * 1000,
+      iconCode: daily.weather_code[index],
+      maxTemp: Math.round(daily.temperature_2m_max[index]),
+    };
+  });
+}
+
+function parseHourlyWheather({ hourly, current }) {
+  return hourly.time
+    .map((time, index) => {
+      return {
+        timestamp: time * 1000,
+        iconCode: hourly.weather_code[index],
+        temp: Math.round(hourly.temperature_2m[index]),
+        feelsLike: Math.round(hourly.apparent_temperature[index]),
+        winSpeed: Math.round(hourly.wind_speed_10m[index]),
+        precip: Math.round(hourly.precipitation[index] * 100) / 100,
+      };
+    })
+    .filter(({ timestamp }) => timestamp >= current.time * 1000);
+}
